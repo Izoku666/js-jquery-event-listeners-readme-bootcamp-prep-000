d1b7cfae0146174efae681aa5291b7e4692268ee
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-15 16:10:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function()
+  {
+    return alert("Hey!");
+  });  // have a click event on <p> and alert("Hey!")
+}
+
+function frameIt()
+{
+   $('img').on('load', function()
+   {
+     // add a red frame here
+     $('img').addClass('tasty');
+   });
+}
+
+function pressIt()
+{
+  $('input').on('keydown', function(){
+      if(key.which == 71)
+      {
+        return alert("G has been pressed");
+      }
+  });
+}
+// event.keydown = g
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
 });
